b4b6fb62a60fb37280aa4df770f5fe7d9bc2a7a7
paxest <71469404+paxest@users.noreply.github.com>
2022-03-09 18:36:37 +0300

    added test for product page

--- pages/locators.py
+++ pages/locators.py
@@ -7,3 +7,10 @@ class MainPageLocators:
 class LoginPageLocators:
     LOGIN_FORM = (By.ID, "login_form")
     REG_FORM = (By.ID, "register_form")
+    
+class ProductPageLocators:
+    ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, "#add_to_basket_form > button")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main p.price_color")
+    PRODUCT_DESCRIPTION = (By.CSS_SELECTOR, "#product_description+p")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, ".alertinner strong")
---
+++ pages/product_page.py
@@ -0,0 +1,43 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+class ProductPage(BasePage):
+    product_name = ""
+    product_price = ""
+    
+    def add_product_to_basket(self):
+        self.should_be_product_name()
+        self.should_be_product_price()
+        self.should_be_product_description()
+        self.should_be_add_to_basket_button()
+        
+        add_product = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
+        add_product.click()
+        
+        self.solve_quiz_and_get_code()
+        self.should_be_message_about_adding()
+        self.check_message_about_adding()
+        
+    def should_be_add_to_basket_button(self):
+        assert self.is_element_present(*ProductPageLocators.ADD_TO_BASKET_BUTTON), "Basket button is not presented"
+    
+    def should_be_product_name(self):
+        assert self.is_element_present(*ProductPageLocators.PRODUCT_NAME), "Product name is not presented"
+        self.product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+    
+    def should_be_product_price(self):
+        assert self.is_element_present(*ProductPageLocators.PRODUCT_PRICE), "Product price is not presented"
+        self.product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+        
+    def should_be_product_description(self):
+        assert self.is_element_present(*ProductPageLocators.PRODUCT_DESCRIPTION), "Product description is not presented"
+    
+    def should_be_message_about_adding(self):
+        assert self.is_element_present(*ProductPageLocators.SUCCESS_MESSAGE), "Message about adding product is not presented"
+    
+    def check_message_about_adding(self):
+        msg_lst = self.browser.find_elements(*ProductPageLocators.SUCCESS_MESSAGE)
+        assert len(msg_lst) == 3, "Some messages not presented"
+        
+        assert self.product_name == msg_lst[0].text, "Wrong product name added to basket"
+        assert self.product_price == msg_lst[2].text, "Wronf product price added to basket"
